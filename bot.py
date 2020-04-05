c51b8821861019ad64c8de8892aa21af73579d32
jacob <siggins@protonmail.com>
2020-04-05 05:55:55 +0100

    ¹

---
+++ bot.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+
+class Cidrap():
+    def __init__(self,stdout = None,stderr = None):
+        op = webdriver.ChromeOptions()
+        op.add_argument('headless')
+        self.driver = webdriver.Chrome(options=op)
+
+    def grab(self):
+        self.driver.get('http://www.cidrap.umn.edu/')
+        title = self.driver.find_element_by_xpath('//*[@id="node-183041"]/div/div/div[2]/div/h3/a').get_attribute('innerHTML')
+        print(title)
+        paragraph = self.driver.find_element_by_xpath('//*[@id="node-183041"]/div/div/div[2]/div/div[2]/div/div/p').get_attribute('innerHTML')
+        print(paragraph)
+
+bot = Cidrap()
+bot.grab()
